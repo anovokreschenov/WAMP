b8ed9725e672b79fb40f246ab5594e2944caca80
Alexey Novokreschenov <a.v.novokreschenov@gmail.com>
2022-06-02 10:14:16 +0700

    Update Node.js to 16.15.1

--- install.cmd
+++ install.cmd
@@ -78,15 +78,15 @@ if not exist bin\composer\composer.bat (
 if not exist bin\node\node.exe (
 	if exist bin\node rd /q /s bin\node >nul 2>&1
 	(
-		echo. | set /p v="Installing Node.js 16.15.0..."^
-		&& powershell -Command "$progressPreference = 'silentlyContinue'; Invoke-WebRequest -Method Get -Uri 'https://nodejs.org/dist/v16.15.0/node-v16.15.0-win-x64.zip' -OutFile '%CD%\temp\node.zip';" >nul 2>&1^
+		echo. | set /p v="Installing Node.js 16.15.1..."^
+		&& powershell -Command "$progressPreference = 'silentlyContinue'; Invoke-WebRequest -Method Get -Uri 'https://nodejs.org/dist/v16.15.1/node-v16.15.1-win-x64.zip' -OutFile '%CD%\temp\node.zip';" >nul 2>&1^
 		&& powershell -Command "$progressPreference = 'silentlyContinue'; Expand-Archive -Path '%CD%\temp\node.zip' -DestinationPath '%CD%\temp\node' -Force;" >nul 2>&1^
-		&& move temp\node\node-v16.15.0-win-x64 bin\node >nul 2>&1^
+		&& move temp\node\node-v16.15.1-win-x64 bin\node >nul 2>&1^
 		&& echo SUCCESS
 	) || (
 		echo FAILED & goto quit
 	)
-	set MSVC_RUNTIME_REQUIRED=1
+	rem set MSVC_RUNTIME_REQUIRED=1
 )
 
 if not exist pma/index.php (
